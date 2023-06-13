183be71d611a04ad28b3519bde489aad859aeddf
Iago Fedochenko Piassa <iagofedochenko596@gmail.com>
2023-06-13 17:26:48 -0300

    contratar

---
+++ js/contratar.js
@@ -0,0 +1,8 @@
+var btnOpen = document.querySelector(".open")
+var popUp1 = document.querySelector('.pop-up')
+var all = document.querySelector('.tudo')
+
+function popUp() {
+    popUp1.classList.toggle("fechado")
+    all.classList.toggle('aberto')
+}
